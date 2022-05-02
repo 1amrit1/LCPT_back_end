e7c1b150cb9211d45ac8ebeba35ac5dac6b95056
Ayush <ayushsingh9200@gmail.com>
2022-05-02 19:35:51 -0400

    cors problem

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions = {
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 //routers
