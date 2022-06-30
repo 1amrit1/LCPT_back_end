69ff3a453cfe28e6a07fcc31ae04c52a7c99377a
Ayush <ayushsingh9200@gmail.com>
2022-06-29 20:36:31 -0400

    changes for deployment issue

--- Model/courseModel.js
+++ Model/courseModel.js
@@ -88,7 +88,6 @@ module.exports.getUserCourseDetails = async function (homeId, roleId, userId, re
             })
         }
     })
-    MongoClient.close();
    // console.log("=======> Response == ", response);
     return retFunc;
 }
@@ -109,8 +108,7 @@ module.exports.getUserCompletedCourses = async function (homeId, roleId, userId,
             })
         }
     });
-    MongoClient.close();
-    return retFunc;
+     return retFunc;
 }
 
 
@@ -129,7 +127,6 @@ module.exports.getAllCourses = async function (retFunc) {
             })
         }
     });
-    MongoClient.close();
     return retFunc;
 }
 
