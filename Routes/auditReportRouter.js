a43e642653fe4aeb7f90562594e1a1524e146235
Amrit Singh Sohi <1amritsinghsohi@gmail.com>
2022-07-01 18:26:02 -0700

    home deficiency and home audit report changes

--- Routes/auditReportRouter.js
+++ Routes/auditReportRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // var { addCourseFn, getAllCoursesFn, updateCourseFn, getUserBasedCourseDetails } = require('../Controller/courseController');
-var { sendOganizationSummary, getHomeSummary, getHomeStaffSummData, getOrganizationStaffTemplates, getOrganizationDeficiencyData } = require('../Controller/auditReportController')
+var { sendOganizationSummary, getHomeSummary, getHomeStaffSummData, getOrganizationStaffTemplates, getOrganizationDeficiencyData, getHomeDeficiencyData } = require('../Controller/auditReportController')
 var auditReportRouter = express.Router();
 
 
@@ -10,5 +10,7 @@ auditReportRouter.get('/home-summary/:home_id', getHomeSummary);
 auditReportRouter.get('/home-staff-summary/:home_id', getHomeStaffSummData);
 auditReportRouter.get('/org-template-specific/:org_id', getOrganizationStaffTemplates);
 auditReportRouter.get('/org-missing-courses/:org_id', getOrganizationDeficiencyData);
+auditReportRouter.get('/home-missing-courses/:homeId', getHomeDeficiencyData);
+
 
 module.exports = auditReportRouter;
